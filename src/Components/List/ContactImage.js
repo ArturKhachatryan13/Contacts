3d1c9ade0d79acfb2337dd54aafd15eff64f8fa8
Artur Khachatryan <arturkhachatryan@Vahagns-Mac-mini.local>
2022-07-08 17:07:55 +0400

    refactor (Contacts List): the changes on the most recent review

--- src/Components/List/ContactImage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import UserInitial from './UserInitial';
-import UserPicture from './UserPicture';
-
-const ContactImage = ({ contact }) => {
-  return contact.photo ? <UserPicture /> : <UserInitial contact={contact} />;
-};
-
-export default ContactImage;
